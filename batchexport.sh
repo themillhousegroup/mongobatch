867ebd71e8608746d5f9c9099fff76bc2e11c3a0
John Marshall <john@themillhousegroup.com>
2016-05-14 13:43:45 +1000

    Use functions defined in config.sh. Support single-file processing

--- batchexport.sh
+++ batchexport.sh
@@ -3,11 +3,12 @@
 if [ $# -eq 0 ]
 then
 	echo "Usage:"
-	echo "batchexport.sh <config-dir> <dump-base>"
+	echo "batchexport.sh <config-dir> <dump-base> [db-name]"
 	echo
 	echo "Arguments:"
 	echo "<config-dir> the absolute path to the place where config files are held"
 	echo "<dump-base>  the absolute path to the place where dumps should be stored"
+  echo "[db-name]    (optional) only process collections in the given databse"
 	echo
 	echo "this path will have db_name/DATE directories put into it"
 	echo
@@ -26,26 +27,28 @@ fi
 
 CONFIG_DIR=$1
 DUMP_BASE=$2
+DB_NAME=$3
 
 DATE=`date "+%Y/%m/%d"`
 
-for f in ${CONFIG_DIR}/*
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/config.sh"
+
+listfiles ${CONFIG_DIR} ${DB_NAME}
+
+echo "Processing: ${CONFIG_FILES}"
+
+for f in ${CONFIG_FILES} 
 do
-	# Read into array called lines:
-	IFS=$'\n' read -d '' -r -a lines < $f
-		
-	HOST=${lines[0]}
-	# Trim the db name as it's used as part of a filename:
-	DB_NAME=`echo ${lines[1]} | xargs`
+
+  readsinglefile $f
 	
 	DUMP_DIR="${DUMP_BASE}/${DB_NAME}/${DATE}"
 	mkdir -p $DUMP_DIR 
 	
 	echo
 	echo "Performing Mongo Export to directory ${DUMP_DIR}"
-
-	# lines 4-N are the names of the collections - slice it:
-	COLLECTIONS=("${lines[@]:4}") 
 
 	for collection in "${COLLECTIONS[@]}"
 	do
@@ -55,7 +58,7 @@ do
 
 		echo "Starting Mongo Export to $FILE_LOC"
 
-		mongoexport -h ${HOST} -d ${DB_NAME} -c ${collection} -u ${lines[2]} -p ${lines[3]} -o ${FILE_LOC}
+		mongoexport -h ${HOST} -d ${DB_NAME} -c ${collection} -u ${USERNAME} -p ${PASSWORD} -o ${FILE_LOC}
 	done
 	echo
 	echo "Completed Mongo Export to directory ${DUMP_DIR}"
